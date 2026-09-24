57a360ae7b91243de0e51d246bfa83b3e1acf8c2
Matthew Alvis <mrmattalvis@gmail.com>
2026-09-24 19:15:34 -0400

    part 2 complete

--- TripCalculator/Program.cs
+++ TripCalculator/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("How many miles for your trip? ");
+﻿
+Console.WriteLine("How many miles for your trip? ");
 double tripMiles = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("How many miles per gallon can you go? ");
@@ -17,3 +18,24 @@ double fuelCost = gallonsNeeded * pricePerGallon;
 //Print the calculations
 System.Console.WriteLine("Gallons needed: " + gallonsNeeded.ToString("F2"));
 System.Console.WriteLine("Fuel Cost: " + fuelCost.ToString("C"));
+
+//Part 2
+
+Console.WriteLine("How many people are going? ");
+double peopleGoing = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("How many pizzas? ");
+double pizza = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("How much does a pizza slice cost? ");
+double pricePerSlice = Convert.ToDouble(Console.ReadLine());
+
+//const double slices = 8;
+
+double slices = pizza * 8;
+double slicePerPerson = slices / peopleGoing;
+double pizzaCost = pricePerSlice * slices;
+
+System.Console.WriteLine("Total slices: " + slices);
+System.Console.WriteLine("Slices per Person: " + slicePerPerson.ToString("F1"));
+System.Console.WriteLine("Pizza Cost: " + pizzaCost.ToString("F2"));
